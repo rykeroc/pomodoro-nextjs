66580341157e1df95f086ca58d1c35b30d147fd1
ryker <rykercooke@gmail.com>
2024-11-30 12:12:52 -0500

    Update common.ts

--- src/app/_components/common.ts
+++ src/app/_components/common.ts
@@ -4,6 +4,15 @@ const fadeTransitionClasses: string[] = [
 	'duration-200'
 ]
 
+const glassEffectClasses: string[] = [
+	"border",
+	"border-primary-container",
+	"bg-gradient-to-br",
+	"from-primary-container",
+	"backdrop-blur-sm",
+]
+
 export {
-	fadeTransitionClasses
+	fadeTransitionClasses,
+	glassEffectClasses
 }
