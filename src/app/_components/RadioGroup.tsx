adb3b39fa2f7c3df411e1721949e255489a82849
ryker <rykercooke@gmail.com>
2024-11-30 10:48:44 -0500

    Update RadioGroup.tsx: Change clsx usage -> cx from cva

--- src/app/_components/RadioGroup.tsx
+++ src/app/_components/RadioGroup.tsx
@@ -1,6 +1,6 @@
 import {RadioGroup as HeadlessUiRadioGroup, Radio as HeadlessUiRadio, Field} from "@headlessui/react";
-import clsx from "clsx";
 import {fadeTransitionClasses} from "@/app/_components/common";
+import {cx} from "class-variance-authority";
 
 interface RadioProps {
 	value: string
@@ -15,35 +15,39 @@ interface RadioGroupProps {
 const RadioGroup = ({items, selected, setSelected}: RadioGroupProps) => {
 
 	const Radio = ({value}: RadioProps) => (
-		<div className={clsx(
-			'flex flex-row gap-3 items-center',
+		<div className={cx([
+			"flex", "flex-row", "gap-3", "items-center",
 			"hover:brightness-125",
-			fadeTransitionClasses
-		)}>
+			...fadeTransitionClasses
+		])}>
 			<HeadlessUiRadio
 				value={value}
-				className={clsx("group flex size-6 items-center justify-center",
-					"rounded-full ring-2 ring-inset ring-secondary-text",
-					"data-[checked]:ring-primary")}
-			>
-				<span className={clsx(
-					"invisible size-3 rounded-full bg-primary",
+				className={cx([
+					"group", "peer", "flex size-6", "items-center", "justify-center",
+					"rounded-full", "ring-2", "ring-inset", "ring-secondary-text",
+					"data-[checked]:ring-primary"
+				])}>
+				<span className={cx([
+					"invisible", "size-3", "rounded-full", "bg-primary",
 					"group-data-[checked]:visible"
-				)}/>
+				])}/>
 			</HeadlessUiRadio>
 
-			<label className={clsx({"text-secondary-text": selected !== value})}>{value}</label>
+			<label className={cx("text-secondary-text", "peer-data-[checked]:text-primary-text")}>{value}</label>
 		</div>
 	)
 
+	const radioButtons = items.map(i => <Field key={i}> <Radio value={i}/></Field>)
 
-	return <HeadlessUiRadioGroup value={selected} onChange={setSelected} className={clsx(
-		'flex flex-col gap-3'
-	)}>
-		{
-			items.map(i => <Field key={i}>	<Radio value={i}/></Field>)
-		}
-	</HeadlessUiRadioGroup>
+	return (
+		<HeadlessUiRadioGroup
+			value={selected} onChange={setSelected}
+			className={cx([
+				"flex", "flex-col", "gap-3"
+			])}>
+			{radioButtons}
+		</HeadlessUiRadioGroup>
+	)
 }
 
 export default RadioGroup
