b6758d37beb8a6d9cac3783d6ac2b1cf9cdcd33e
ryker <rykercooke@gmail.com>
2024-11-30 13:06:00 -0500

    Add PomodoroTimerIndicator.tsx

---
+++ src/app/_components/PomodoroTimerIndicator.tsx
@@ -0,0 +1,101 @@
+import Button from "@/app/_components/Button";
+import {ChevronRightIcon} from "@heroicons/react/16/solid";
+import {cx} from "class-variance-authority";
+
+function secondsToMinutes(seconds: number): string {
+	const minutesStr = Math.floor(seconds / 60).toString().padStart(2, "0")
+	const secondsStr = (seconds % 60).toString().padStart(2, "0")
+	return `${minutesStr}:${secondsStr}`
+}
+
+interface PomodoroTimerIndicatorProps {
+	seconds: number
+	maxSeconds: number
+	label?: string
+	hideLabel?: boolean
+	taskName?: string,
+}
+
+const PomodoroTimerIndicator = (
+	{
+		seconds,
+		maxSeconds,
+		label = "",
+		hideLabel = true,
+		taskName = "Focus"
+	}: PomodoroTimerIndicatorProps) => {
+	const size = 450
+	const strokeWidth = 8
+	const viewBox = `0 0 ${size} ${size}`
+	const radius = (size - strokeWidth) / 2
+	const dashArray = radius * Math.PI * 2
+	const percentage = Math.min(100, (seconds / maxSeconds) * 100)
+	const dashOffset = dashArray - (dashArray * percentage) / 100
+	const minutesString = secondsToMinutes(seconds)
+	return (
+		<svg
+			width={size} height={size} viewBox={viewBox}>
+			{/* Background */}
+			<defs>
+				<linearGradient id={"timerGradient"} x1="0%" y1="0%" x2="100%" y2="100%" className={"backdrop-blur-sm"}>
+					<stop offset="0%" stopColor="#73767E"/>
+					<stop offset="100%" stopColor="transparent"/>
+				</linearGradient>
+			</defs>
+			<circle
+				fill={"url(#timerGradient)"}
+				cx={size / 2}
+				cy={size / 2}
+				r={radius}
+			/>
+			{/* Background circle */}
+			<circle
+				className={"fill-none stroke-secondary-text"}
+				cx={size / 2}
+				cy={size / 2}
+				strokeWidth={`${strokeWidth - 1}px`}
+				r={radius}
+			/>
+			{/* Progress circle*/}
+			<circle
+				className={"fill-none stroke-primary transition delay-200 ease-in"}
+				cx={size / 2}
+				cy={size / 2}
+				r={radius}
+				strokeLinecap={"butt"}
+				strokeWidth={`${strokeWidth}px`}
+				strokeDasharray={dashArray}
+				strokeDashoffset={dashOffset}
+				transform={`rotate(-90 ${size / 2} ${size / 2})`}
+			/>
+
+			<foreignObject
+				x={"0%"}
+				y={"0%"}
+				width={size}
+				height={size}>
+				<div
+					className={"flex flex-col items-center justify-center w-full h-full gap-3"}>
+					<h4
+						className={cx(
+							["text-primary-text"],
+							{"invisible": hideLabel}
+						)}>
+						{label}
+					</h4>
+					<h1>
+						{minutesString}
+					</h1>
+					<Button>
+						<h3 className={"text-secondary-text"}>
+							{taskName}
+						</h3>
+						<ChevronRightIcon className={"size-8 fill-secondary-text"}/>
+					</Button>
+				</div>
+			</foreignObject>
+		</svg>
+	)
+}
+
+export default PomodoroTimerIndicator
