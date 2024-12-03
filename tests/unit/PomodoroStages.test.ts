ed8923b145affb8518e2473e230c8bf66a85feb5
ryker <rykercooke@gmail.com>
2024-12-03 12:45:20 -0500

    Add PomodoroStages.test.ts

---
+++ tests/unit/PomodoroStages.test.ts
@@ -0,0 +1,33 @@
+import {describe, expect, test} from "@jest/globals";
+import {PomodoroStages} from "../../src/app/_lib/constants/PomodoroStage";
+
+/*
+Tests to ensure that each stage has the proper duration.
+
+This is important for development when the duration may be shortened for
+testing purposes.
+ */
+describe('Pomodoro stages duration', () => {
+
+	test('Focus session - 25 minutes', () => {
+		const duration = 25 * 60
+		expect(
+			PomodoroStages.focusSession.seconds
+		).toEqual(duration)
+	})
+
+	test('Short break - 5 minutes', () => {
+		const duration = 5 * 60
+		expect(
+			PomodoroStages.shortBreak.seconds
+		).toEqual(duration)
+	})
+
+	test('Long break - 15 minutes', () => {
+		const duration = 15 * 60
+		expect(
+			PomodoroStages.longBreak.seconds
+		).toEqual(duration)
+	})
+
+})
