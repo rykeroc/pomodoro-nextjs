ee7b95dac3272506a28cf298ad1fce4e38ded9f2
ryker <rykercooke@gmail.com>
2024-12-03 18:31:02 -0500

    Add providers.tsx
    Update layout.tsx: Use Providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,27 @@
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import "./globals.css";
 import {Montserrat} from 'next/font/google'
 import {cn} from "@/app/_lib/cn";
+import Providers from "@/app/providers";
 
 const montserrat = Montserrat({
-  subsets: ['latin'],
-  display: 'swap',
+	subsets: ['latin'],
+	display: 'swap',
 })
 
 export const metadata: Metadata = {
-  title: "Focus Cafe",
-  description: "Pomodoro timer app to manage focus duration.",
+	title: "Focus Cafe",
+	description: "Pomodoro timer app to manage focus duration.",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={cn(montserrat.className, 'antialiased')}>
-        {children}
-      </body>
-    </html>
-  );
+export default function RootLayout({children,}: Readonly<{ children: React.ReactNode; }>) {
+	return (
+		<html lang="en">
+		<body className={cn(montserrat.className, 'antialiased')}>
+			<Providers>
+				{children}
+			</Providers>
+		</body>
+		</html>
+	);
 }
