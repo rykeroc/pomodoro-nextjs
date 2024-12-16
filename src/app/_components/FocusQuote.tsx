8cfbf4fe0bc103b838708a840e56d15a20ad4e14
ryker <rykercooke@gmail.com>
2024-12-16 13:49:43 -0500

    Fix FocusQuote.tsx

--- src/app/_components/FocusQuote.tsx
+++ src/app/_components/FocusQuote.tsx
@@ -5,9 +5,20 @@ import useQuoteQuery from "@/app/_lib/hooks/useQuoteQuery";
 
 export default function FocusQuote() {
 	const quoteQuery = useQuoteQuery()
+	if (!quoteQuery.data) return null
+
+	const quote = `"${quoteQuery.data.q}"`
 	return (
-		<h6 className={cn("text-secondary-text", "py-6", "px-12", "text-center",)}>
-			{quoteQuery.data ? `"${quoteQuery.data}"` : ''}
-		</h6>
+		<div className={cn(
+			"fixed", "z-10",
+			"w-full", "h-full", "py-6", "px-12",
+			"flex", "flex-row", "justify-center", "items-end"
+		)}>
+			<h6 className={cn(
+				"text-secondary-text", "text-center",
+			)}>
+				{quote}
+			</h6>
+		</div>
 	)
 }
