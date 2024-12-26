fff61a9405778434b9c0747cc793ca146a19f9a8
ryker <rykercooke@gmail.com>
2024-12-26 15:26:23 -0500

    Add ProviderButtons.tsx

---
+++ src/app/signin/_components/ProviderButtons.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import Button from "@/app/_components/inputs/Button";
+import {signIn} from "next-auth/react";
+import Image from "next/image";
+import {cn} from "@/app/_lib/utils/cn";
+
+interface SocialProvider {
+	name: string
+	logo: string
+	logoAlt: string
+}
+
+export default function ProviderButtons() {
+	const logoSize = 24
+
+	const providers: SocialProvider[] = [
+		{
+			name: "GitHub",
+			logo: "github-mark.svg",
+			logoAlt: "GitHub logo"
+		},
+		// {
+		// 	name: "Google",
+		// 	logo: "google-g.svg",
+		// 	logoAlt: "Google logo"
+		// }
+	]
+
+	const providerButtons = providers.map(p => (
+		<Button key={p.name} onClick={() => signIn(p.name.toLowerCase())} variant={"none"}>
+			<Image src={`/logos/${p.logo}`} alt={p.logoAlt} width={logoSize} height={logoSize}/>
+			<p>Sign in with {p.name}</p>
+		</Button>
+	))
+
+	return (
+		<div className={cn("flex", "flex-col", "gap-2")}>
+			{providerButtons}
+		</div>
+	)
+}
