78f9ba715cefa62436e3bf57798c5e1ff946c934
rykeroc <rykercooke@gmail.com>
2025-01-08 13:06:27 -0500

    Update AccountSection.tsx: Add different elements when signed in vs signed out

--- src/app/_components/TopBarMenus/SettingsDialog/sections/AccountSection.tsx
+++ src/app/_components/TopBarMenus/SettingsDialog/sections/AccountSection.tsx
@@ -1,24 +1,54 @@
 "use client"
 
-import {ArrowRightStartOnRectangleIcon, UserIcon} from "@heroicons/react/24/solid";
+import {ArrowRightEndOnRectangleIcon, ArrowRightStartOnRectangleIcon, UserIcon} from "@heroicons/react/24/solid";
 import Button from "@/app/_components/inputs/Button";
-import {signOut, useSession} from "next-auth/react";
+import {signOut, signIn, useSession} from "next-auth/react";
 import {ISettingsMenuSection} from "@/app/_components/TopBarMenus/SettingsDialog/SettingsMenuSections";
+import {User} from "next-auth";
 
 const AccountSectionContent = () => {
 	const { data: session } = useSession()
 
-	const handleSignOut = () => signOut({redirectTo: "/sign-in"})
+	const content = session?.user ? <SignedInContent user={session.user}/> : <SignedOutContent/>
 
 	return (
 		<div className={"flex flex-col gap-4"}>
-			<h5>Welcome back {session?.user?.name}</h5>
+			{content}
+		</div>
+	)
+}
+
+interface ISignedInContentProps {
+	user: User
+}
+
+function SignedInContent({user}: ISignedInContentProps) {
+	const handleSignOut = () => signOut({redirectTo: "/sign-in"})
+
+	return (
+		<>
+			<p>Welcome back {user?.name}</p>
 
 			<Button onClick={handleSignOut} variant={"secondary"}>
 				<ArrowRightStartOnRectangleIcon className={'size-6'}/>
 				Sign out
 			</Button>
-		</div>
+		</>
+	)
+}
+
+function SignedOutContent() {
+	const handleSignIn = () => signIn()
+
+	return (
+		<>
+			<p>Create an account or sign in to help unlock your true potential!</p>
+
+			<Button onClick={handleSignIn} variant={"secondary"}>
+				<ArrowRightEndOnRectangleIcon className={'size-6'}/>
+				Sign in
+			</Button>
+		</>
 	)
 }
 
