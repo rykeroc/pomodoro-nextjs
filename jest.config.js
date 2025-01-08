14c10b7242d339e29dddd87197e93eb0cafa4440
rykeroc <rykercooke@gmail.com>
2025-01-08 13:02:18 -0500

    Update jest.config.js: Add module name mapper to for paths "@/*"
    Update getQuote.test.ts: Skip integration test for GitHub actions workflow

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   testEnvironment: "node",
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
   transform: {
     "^.+.tsx?$": ["ts-jest",{}],
   },
--- tests/integration/getQuote.test.ts
+++ tests/integration/getQuote.test.ts
@@ -1,7 +1,7 @@
 import {describe, expect, test} from "@jest/globals";
 import getQuote from "../../src/app/_lib/actions/getQuote";
 
-describe("getQuote action", () => {
+describe.skip("getQuote action", () => {
 
 	test("Success", async () => {
 		const responseData = await getQuote()
