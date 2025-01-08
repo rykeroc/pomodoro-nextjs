f13eea92616c78ec43fadc7a0b244ee36452aa30
rykeroc <rykercooke@gmail.com>
2025-01-08 12:19:02 -0500

    Add quotes.sql

---
+++ sql/quotes.sql
@@ -0,0 +1,10 @@
+INSERT INTO quotes (text, author)
+VALUES
+    ('Do what you can. Want what you have. Be who you are.', null),
+    ('Happiness is found in doing, not merely possessing.', null),
+    ('Time you enjoy, was not time wasted.', null),
+    ('Life happens in the river, not the shore.', null),
+    ('Even though you are on the right track - you will get run over if you just sit there.', null),
+    ('The key to success is failure.', null),
+    ('Make peace within, and there will be no one who can overcome you. And no one you will wish to overcome.', null),
+    ('Stop saying I wish, start saying I will', null)
