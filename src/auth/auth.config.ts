81ad123b51cedc8ad89942f22b621c2d39026cfd
rykeroc <rykercooke@gmail.com>
2025-01-09 11:49:56 -0500

    Add Spotify to auth.config.ts
    Add spotify-logo.svg
    
    Rename ProviderButtons.tsx -> LoginProviderButtons.tsx
    
    Add Spotify to LoginProviderButtons.tsx.tsx

--- src/auth/auth.config.ts
+++ src/auth/auth.config.ts
@@ -1,12 +1,14 @@
 import {NextAuthConfig} from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import Spotify from "next-auth/providers/spotify";
 
 export default {
 	providers: [
 		GitHub({
 			clientId: process.env.AUTH_GITHUB_ID,
-			clientSecret: process.env.AUTH_GITHUB_SECRET
+			clientSecret: process.env.AUTH_GITHUB_SECRET,
+			allowDangerousEmailAccountLinking: true
 		}),
 		Google({
 			clientId: process.env.AUTH_GOOGLE_CLIENT_ID,
@@ -17,7 +19,13 @@ export default {
 					access_type: "offline",
 					response_type: "code"
 				}
-			}
+			},
+			allowDangerousEmailAccountLinking: true
+		}),
+		Spotify({
+			clientId: process.env.AUTH_SPOTIFY_CLIENT_ID,
+			clientSecret: process.env.AUTH_SPOTIFY_CLIENT_SECRET,
+			allowDangerousEmailAccountLinking: true
 		})
 	],
 } satisfies NextAuthConfig
