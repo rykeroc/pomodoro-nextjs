99d68a154b8b618d0009fcb4aafe0214322db000
rykeroc <rykercooke@gmail.com>
2025-01-09 10:35:10 -0500

    Update auth.config.ts: Add Google provider
    Update ProviderButtons.tsx: Enable Google sign in buttons

--- src/auth/auth.config.ts
+++ src/auth/auth.config.ts
@@ -1,11 +1,23 @@
-import GitHub from "next-auth/providers/github";
 import {NextAuthConfig} from "next-auth";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export default {
 	providers: [
 		GitHub({
 			clientId: process.env.AUTH_GITHUB_ID,
 			clientSecret: process.env.AUTH_GITHUB_SECRET
+		}),
+		Google({
+			clientId: process.env.AUTH_GOOGLE_CLIENT_ID,
+			clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET,
+			authorization: {
+				params: {
+					prompt: "consent",
+					access_type: "offline",
+					response_type: "code"
+				}
+			}
 		})
 	],
 } satisfies NextAuthConfig
